f332b792e9c4f2a1c73e29e3fd2eab657d406ee2
es404020 <es404020@gmail.com>
2022-04-26 11:11:05 +0100

    fix login bug

--- Redux/Auth/authaction.js
+++ Redux/Auth/authaction.js
@@ -1,11 +1,17 @@
 import { signInPending, signInSuccess, signInFailed } from "./authslice";
 import { userSignin, userSignIn } from "../../api/userApi";
+import {parseJwt} from '../../helpers/api'
 export const signIn = (user) => async (dispatch) => {
 
     dispatch(signInPending())
     try {
         const users = await userSignIn(user);
-        dispatch(signInSuccess(users))
+        const result = {
+            user: parseJwt(users.access_token),
+            token: users.access_token,
+          };
+
+    dispatch(signInSuccess(result))
     }
     catch (e) {
 
